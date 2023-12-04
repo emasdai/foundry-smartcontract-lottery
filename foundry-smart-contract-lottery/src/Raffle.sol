0b06002560a5c72447f9aca80c6a243dd412568e
emas dai <dikifarista61@gmail.com>
2023-12-04 05:19:11 +0000

    add raffle.sol

---
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -0,0 +1,19 @@
+// SPDX-license-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+    *   @title  a sample raffle contract
+    *   @author emasdai
+    *   @notice this contract for creating sample raffle
+    *   @dev    implement chainlin VRFv2 
+ */
+contract Raffle {
+    function enterRaffle() public{
+
+    }
+
+    function pickWinner() public{
+        
+    }
+}
