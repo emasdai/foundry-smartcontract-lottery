c70f9803552608584e033c62b4df872b4380a7c7
emas dai <dikifarista61@gmail.com>
2023-12-04 05:45:04 +0000

    add raffle.sol error revert

--- foundry-smart-contract-lottery/src/Raffle.sol
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -1,5 +1,26 @@
-// SPDX-license-Identifier: MIT
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
 
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+// SPDX-license-Identifier: MIT
 pragma solidity ^0.8.18;
 
 /**
@@ -9,7 +30,7 @@ pragma solidity ^0.8.18;
     *   @dev    implement chainlin VRFv2 
  */
 contract Raffle {
-
+    error Raffle__NotEnoughEthSend();   // digunakan jika eth yang di sent kurang, error digunakan untuk efisiensi gas fee, menggunakan awalan nama contract agar mudah diketahui saat error
     uint256 private immutable i_entranceFee;  // i_ adalah immutable, untuk menghemat gas Fee
 
     // constuctor dieksekusi pada saat pembuatan kontrak
@@ -17,7 +38,13 @@ contract Raffle {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable{}
+    // menggunakan external untuk gas fee lebih efisien
+    function enterRaffle() external payable{
+        
+        if(msg.value < i_entranceFee){  //  jika msg.value(Eth) kurang dari i_entranceFee, maka akan revert ke NotEnoughEthSend
+            revert Raffle__NotEnoughEthSend(); 
+        }
+    }
 
     function pickWinner() public{}
 
